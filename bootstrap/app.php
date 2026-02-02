b637169e4075fcef52f53c3e8aefe78f147cac16
catpatnova <Oskar.Vaht@gmail.com>
2026-02-02 11:00:04 +0200

    02.02.2026

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Console\Commands\TimetableNotification;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+    $schedule->command(TimetableNotification::class)->everyMinute();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
